900f8f245c12f53e558588b3b9fc1cff3c63f7d6
yugamxps <yugam.xportsoft@gmail.com>
2022-07-14 15:04:31 +0530

    functional update form

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,12 +3,24 @@ import { data } from "../../../data";
 
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
   return (
     <>
       {people.map((person) => {
-        console.log(person);
-        return "Hello";
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>Remove</button>
+          </div>
+        );
       })}
+      <button className="btn" onClick={() => setPeople([])}>
+        Clear Items
+      </button>
     </>
   );
 };
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    message: "random message",
+  });
+
+  const [name, setName] = useState("peter");
+  const [age, setAge] = useState("24");
+  const [message, setMessage] = useState("random message");
+
+  const changeMessage = () => {
+    // setPerson({ ...person, message: "hello world" });
+    setMessage("Hello world");
+  };
+  return (
+    <>
+      <h2>{name}</h2>
+      <h2>{age}</h2>
+      <h2>{message}</h2>
+      <button className="btn" onClick={changeMessage}>
+        Change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
