fd64b214925b9d27f38ed887469cd3c82cefce10
yugamxps <yugam.xportsoft@gmail.com>
2022-08-09 17:20:11 +0530

    started form basics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 // import Setup from "./tutorial/1-useState/setup/1-error-example";
 // import Setup from "./tutorial/1-useState/setup/2-useState-basics";
 // import Setup from "./tutorial/1-useState/setup/3-useState-array";
@@ -7,13 +7,16 @@ import React from 'react'
 // import Setup from "./tutorial/2-useEffect/setup/1-useEffect-basics";
 // import Setup from "./tutorial/2-useEffect/setup/2-useEffect-cleanup";
 // import Setup from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
-import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns'
+// import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns';
+// import Setup from './tutorial/3-conditional-rendering/setup/2-short-circuit';
+// import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide';
+import Setup from './tutorial/4-forms/setup/1-controlled-inputs';
 function App() {
   return (
     <div className='container'>
       <Setup />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,13 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  return (
+    <>
+      <article>
+        <form action=' '></form>
+      </article>
+    </>
+  );
 };
 
 export default ControlledInputs;
